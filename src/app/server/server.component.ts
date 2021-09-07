1edf279a752299f4f13b851bc5009c323e7aadc0
Daniel Fontoura <fontouradaniel@hotmail.com>
2021-09-07 11:53:24 -0300

    second commit - so far I learned the basiscs of components, directives and others

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,7 +1,32 @@
+import { getCurrencySymbol } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `,
+  ],
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId = 10;
+  serverStatus = '';
+
+  constructor() {
+    Math.random() > 0.5
+      ? (this.serverStatus = 'online')
+      : (this.serverStatus = 'offline');
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+}
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  allowServerCreation = false;
+  serverName = 'Testserver';
+  serverCreated = false;
+  servers = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    setTimeout(() => {
+      this.allowServerCreation = true;
+    }, 2000);
   }
 
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.servers.push(this.serverName);
+    this.serverCreated = true;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
